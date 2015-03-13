6e90d07552269bc50e7bfdbd71df362c6e40dc2f
tomsnor <tommartens21@hotmail.com>
2015-03-13 12:20:00 +0100

    Beginselen steganografie

--- Java/src/main/java/be/pxl/encryption/Test.java
+++ Java/src/main/java/be/pxl/encryption/Test.java
@@ -1,5 +1,30 @@
-package be.pxl.encryption;
+package be.pxl.basicSecurity;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Test {
+	
+		
+	public static void main(String[] args) {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("praisethelord.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(img.getRGB(0,0));
+		System.out.println(Integer.toString(img.getRGB(0,0)));
+		
+		//int test = 
+		
+		//img.setRGB(0, 0,);
+		
+		
+	}
 
 }
